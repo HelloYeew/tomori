69ef773723423fc5027bdc19656f8f20c8d1543c
Phawit Pornwattanakul <me@helloyeew.dev>
2025-07-01 02:12:02 +0700

    Add test for reactive object

--- Tomori.Framework.Tests/Reactive/ReactiveTest.cs
+++ Tomori.Framework.Tests/Reactive/ReactiveTest.cs
@@ -10,30 +10,33 @@ namespace Tomori.Framework.Tests.Reactive;
 [TestFixture]
 public class ReactiveTest
 {
-    [SetUp]
-    public void Setup()
+    [OneTimeSetUp]
+    public void InitializeLogger()
     {
         Logger.Initialize();
+    }
+
+    [OneTimeTearDown]
+    public void ShutdownLogger()
+    {
+        Logger.Shutdown();
     }
 
     [Test]
     public void TestInitialValue()
     {
         var reactive = new Reactive<int>(10);
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(reactive.Value, Is.EqualTo(10));
-            Assert.That(reactive.Default, Is.EqualTo(10));
-        }
+        Assert.That(reactive.Value, Is.EqualTo(10));
+        Assert.That(reactive.Default, Is.EqualTo(10));
     }
 
     [Test]
     public void TestValueChangedEvent()
     {
-        string startValue = "anon";
-        var reactive = new Reactive<string>(startValue);
-        string eventOldValue = null;
-        string eventNewValue = null;
+        const string start_value = "anon";
+        var reactive = new Reactive<string>(start_value);
+        string? eventOldValue = null;
+        string? eventNewValue = null;
         int firedCount = 0;
 
         reactive.ValueChanged += e =>
@@ -47,15 +50,106 @@ public class ReactiveTest
         using (Assert.EnterMultipleScope())
         {
             Assert.That(firedCount, Is.EqualTo(1));
-            Assert.That(eventOldValue, Is.EqualTo(startValue));
+            Assert.That(eventOldValue, Is.EqualTo(start_value));
             Assert.That(eventNewValue, Is.EqualTo("anon tokyo"));
             Assert.That(reactive.Value, Is.EqualTo("anon tokyo"));
         }
 
-        Logger.LogPrint("Current value: " + firedCount);
-
-        // Fire again with the same value
         reactive.Value = "anon tokyo";
         Assert.That(firedCount, Is.EqualTo(1), "ValueChanged should not fire when the value does not change");
+
+        reactive.Value = "anon tokyo 2";
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(firedCount, Is.EqualTo(2));
+            Assert.That(eventOldValue, Is.EqualTo("anon tokyo"));
+            Assert.That(eventNewValue, Is.EqualTo("anon tokyo 2"));
+            Assert.That(reactive.Value, Is.EqualTo("anon tokyo 2"));
+        }
+    }
+
+    [Test]
+    public void TestBindingValueSingle()
+    {
+        var source = new Reactive<int>(5);
+        var target = new Reactive<int>(0);
+
+        target.BindTo(source);
+
+        source.Value = 10;
+        Assert.That(target.Value, Is.EqualTo(10), "target value changed after bind");
+        Assert.That(source.Value, Is.EqualTo(10), "source value changed normally");
+
+        target.Value = 20;
+        Assert.That(source.Value, Is.EqualTo(10), "source value not changed by target");
+        Assert.That(target.Value, Is.EqualTo(20), "target value changed after target set");
+    }
+
+    [Test]
+    public void TestBindingValueMultiple()
+    {
+        var source = new Reactive<int>(5);
+        var target1 = new Reactive<int>(0);
+        var target2 = new Reactive<int>(0);
+
+        target1.BindTo(source);
+        target2.BindTo(source);
+
+        source.Value = 10;
+        Assert.That(target1.Value, Is.EqualTo(10), "target1 value changed after bind");
+        Assert.That(target2.Value, Is.EqualTo(10), "target2 value changed after bind");
+        Assert.That(source.Value, Is.EqualTo(10), "source value changed normally");
+    }
+
+    [Test]
+    public void TestBindingMultipleSources()
+    {
+        var source1 = new Reactive<int>(5);
+        var source2 = new Reactive<int>(10);
+        var target = new Reactive<int>(0);
+
+        target.BindTo(source1);
+        target.BindTo(source2);
+
+        source1.Value = 15;
+        Assert.That(target.Value, Is.EqualTo(15), "target value changed after first source bind");
+
+        source2.Value = 20;
+        Assert.That(target.Value, Is.EqualTo(20), "target value changed after second source bind");
+    }
+
+    [Test]
+    public void TestUnbindSpecificSource()
+    {
+        var source = new Reactive<int>(5);
+        var target = new Reactive<int>(0);
+
+        target.BindTo(source);
+        source.Value = 10;
+        Assert.That(target.Value, Is.EqualTo(10), "target value changed after bind");
+
+        target.UnbindFrom(source);
+        source.Value = 20;
+        Assert.That(target.Value, Is.EqualTo(10), "target value did not change after unbind");
+    }
+
+    [Test]
+    public void TestUnbindAllSources()
+    {
+        var source1 = new Reactive<int>(5);
+        var source2 = new Reactive<int>(10);
+        var target = new Reactive<int>(0);
+
+        target.BindTo(source1);
+        target.BindTo(source2);
+
+        source1.Value = 15;
+        source2.Value = 20;
+        Assert.That(target.Value, Is.EqualTo(20), "target value changed after both binds");
+
+        target.UnbindAll();
+        source1.Value = 25;
+        source2.Value = 30;
+        Assert.That(target.Value, Is.EqualTo(20), "target value did not change after unbind all");
     }
 }
